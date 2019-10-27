ccdedc786d48586bca3de83ab2e8800154400811
samerlagha <samer_la@mail.ru>
2019-10-27 09:58:32 +0200

    index.js update

--- lesson2/numberInconsol/index.js
+++ lesson2/numberInconsol/index.js
@@ -3,6 +3,20 @@ let b=2;
 console.log('a: ', a);
 console.log('b: ', b);
 console.log( 'a-b = ','[', (a-b),']');
-console.log('a+b = ' + (a+b));
-console.log('a/b = ' +( a/b));
-console.log('a*b = ' + (a*b));
+console.log( 'a+b = ','[', (a+b),']');
+console.log( 'a/b = ','[', (a/b),']');
+console.log( 'a*b = ','[', (a*b),']');
+
+// console.log('a: '+ a);
+// console.log('b: '+ b);
+// console.log( 'a-b = '+ (a-b));
+// console.log('a+b = ' + (a+b));
+// console.log('a/b = ' +( a/b));
+// console.log('a*b = ' + (a*b));
+
+// console.log('a: ', a);
+// console.log('b: ', b);
+// console.log( 'a-b = ', (a-b));
+// console.log('a+b = ' , (a+b));
+// console.log('a/b = ' ,( a/b));
+// console.log('a*b = ' , (a*b));
