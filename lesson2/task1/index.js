650d040380bb350f9a378ff139487c74a9c6d718
samerlagha <samer_la@mail.ru>
2019-10-28 21:01:50 +0200

    update file

--- lesson2/task1/index.js
+++ lesson2/task1/index.js
@@ -1,8 +1,11 @@
-let a=4;
-let b=2;
-console.log('a: ', [a]);
-console.log('b: ',[b]);
-console.log( 'a-b = ',[ a-b]);
-console.log('a+b = ' , [a+b]);
-console.log('a/b = ' , [a/b]);
+let goodNmber = 17;
+let anotherNumber = goodNmber;
+anotherNumber = 77;
+console.log(goodNmber);
+console.log(anotherNumber);
+let customer={name:'John',age: 17};
+let anotherCustomer = customer;
+anotherCustomer.age = 20;
+console.log(customer.age);
+console.log(anotherCustomer.age);
 console.log('a*b = ' , [a*b]);
