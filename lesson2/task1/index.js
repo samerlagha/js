f1c4102a6f142961ce4fc9081c19e806aec6ddca
samerlagha <samer_la@mail.ru>
2019-10-29 13:47:47 +0200

    dz1 init

--- lesson2/task1/index.js
+++ lesson2/task1/index.js
@@ -1,11 +1,10 @@
-let goodNmber = 17;
-let anotherNumber = goodNmber;
+let goodNumber = 17;
+let anotherNumber = goodNumber;
 anotherNumber = 77;
-console.log(goodNmber);
+console.log(goodNumber);
 console.log(anotherNumber);
 let customer={name:'John',age: 17};
 let anotherCustomer = customer;
 anotherCustomer.age = 20;
 console.log(customer.age);
 console.log(anotherCustomer.age);
-console.log('a*b = ' , [a*b]);
