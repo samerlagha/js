375a52b88ba49350026dc2476d5584c1814b4e42
samerlagha <samer_la@mail.ru>
2019-10-29 17:24:27 +0200

    Update index.js

--- lesson3/dz1/index.js
+++ lesson3/dz1/index.js
@@ -27,10 +27,10 @@ console.log("'text' + null: ", 'text' + null);
 let num = 17;
 // Постфиксная и префиксная форма записи
 ++num;
-console.log("++num: ", num);
+console.log("++num: ", ++num);
 
 num++;
-console.log("num++: ", num);
+console.log("num++: ", num++);
 
 // математические операторы приводят операнды к числам
 const a = 17;
