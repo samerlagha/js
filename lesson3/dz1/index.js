41a1345bd8181015c3c2c647497349264110e1fc
samerlagha <samer_la@mail.ru>
2019-10-29 15:28:27 +0200

    Update index.js

--- lesson3/dz1/index.js
+++ lesson3/dz1/index.js
@@ -24,6 +24,38 @@ console.log("5 + undefined: ", 5 + undefined);
 'text' + null;
 console.log("text + null: ", text + null);
 let num = 17;
+//console.log("num: ", num);
+
 // Постфиксная и префиксная форма записи
 ++num;
-console.log("++num: ", ++null);
+console.log("++num: ", ++num);
+
+num++;
+console.log("num++: ", ++num);
+
+// математические операторы приводят операнды к числам
+const a = 17;
+const b = -a;
+'24' / 4;
+console.log("'24' / 4: ", '24 / 4');
+18 * '3';
+console.log("18 * '3': ",18 * '3');
+'9' - '3';
+console.log("'9' - '3': ",'9' - '3');
+'seventeen' / 3;
+console.log("'seventeen' / 3: ", 'seventeen / 3');
+4 * 'four';
+console.log("4 * 'four': ", 4 * 'four');
+17 / 0;
+console.log("17 / 0: ", 17 / 0);
+
+10 + 'seven';
+console.log("10 + 'seven': ", 10 + 'seven');
+undefined + 1;
+console.log("undefined + 1: ", undefined + 1);
+null + 8;
+console.log("nll + 8");
+undefined + '5';
+console.log("undefined + '5': ", undefined + '5');
+'text' + null;
+console.log("'text' + null: ",'text' + null);
