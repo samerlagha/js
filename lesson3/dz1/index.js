541b6ac8c9c625471e16e0dd36acfa17a28c2903
samerlagha <samer_la@mail.ru>
2019-10-29 15:08:25 +0200

    init dz1

---
+++ lesson3/dz1/index.js
@@ -0,0 +1,31 @@
+// Унарный плюс приводит операнд к числу
++17;
+console.log("+17: ",+17);
++'77';
+console.log("+'77': ",+'77');
+
+// Бинарный плюс складывает строки или прибавляет числа
+10 + '5';
+console.log("5 + '10': ", 10 + '5');
+'some' + 'text';
+console.log("'some' + 'text': ", 'some' + 'text');
+
+// Математические операции с undefined возвращают NaN
+undefined + 1;
+console.log("undefined + 1: ", undefined + 1);
+null + 8;
+console.log("null + 8: ", null + 8);
+undefined + '5';
+console.log("undefined + '5': ", undefined + '5');
+7 - null;
+console.log("7 - null: ", 7 - null);
+5 - undefined;
+console.log("5 + undefined: ", 5 + undefined);
+'text' + null;
+console.log("text + null: ", text + null);
+let num = 17;
+console.log("num: ", num);
+
+// Постфиксная и префиксная форма записи
+++num;
+console.log("++num: ", ++null);
